cbd65f14850c9eb5dc2a88ab1bb01554d1c2c6d3
SergiiProkopyshun <57590966+SergiiProkopyshun@users.noreply.github.com>
2020-10-01 19:52:16 +0300

    Add files via upload

---
+++ Java script/T 1/main.js
@@ -0,0 +1,16 @@
+const name = "Оля";
+
+const last_name = "Дмирірієва";
+
+const product = 'iPhone 11';
+
+const number = 3;
+
+const phone = 0972385406;
+
+const full_name = `${name} ${last_name}`;
+
+console.log(`Вітаю, ${full_name}! Ваше замовлення - ${product} у кількості - ${number} штук успішно додано. На ваш телефон - ${phone} зателефонує наш оператор для підтверждення.`)
+
+
+
