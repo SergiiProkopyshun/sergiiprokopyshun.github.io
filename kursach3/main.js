a5043a64c6ff38fe7e0e3fbfa437eba8f5f32a11
SergiiProkopyshun <57590966+SergiiProkopyshun@users.noreply.github.com>
2022-03-12 15:16:18 +0200

    Add files via upload

---
+++ kursach3/main.js
@@ -0,0 +1,138 @@
+document.addEventListener('DOMContentLoaded', async function(){   
+
+                //Витягуємо темплейти
+                let home = await axios.get("templates/home.html");
+                let login = await axios.get("templates/login.html");
+
+                //Основна інформація для spa (сайту)
+                const data =  {
+                    message: 'Hello Vue.js!',
+                    currentPath: window.location.hash,
+                    user: {},
+                    signIn: false,
+                    logged: false
+                };
+
+                //Компоненти
+                const Home = {
+                    template: home.data,
+                };
+
+                const Login = {
+                    template: login.data,
+                    methods: { 
+                        googleAuth(){
+                            console.log("click")
+
+                            firebase.auth()
+                            .signInWithPopup(provider)
+                            .then( result => {
+                                console.log(result)
+                                const user = result.user;
+
+                                const new_user = {
+                                    displayName: user.displayName,
+                                    email: user.email,
+                                    photo: user.photoURL
+                                }
+                            data.user = new_user;
+                            data.logged = true;
+                            this.$root.$forceUpdate();
+                            window.location.hash = "/home";
+
+                                console.log(new_user);
+                            })
+                            .catch( error => {
+                                console.log(error)
+                            });
+                        }
+                    },
+                    signUpWithPassword(){
+                        console.log("save")
+                        const email = document.getElementById("user-email").value;
+                        const password = document.getElementById("user-password").value;
+                        firebase.auth().createUserWithEmailAndPassword(email, password)
+                        .then((userCredential) => {
+                            var user = userCredential.user;
+                           
+
+                            const new_user = {
+                                displayName: user.displayName,
+                                email: user.email,
+                                photo: user.photoURL
+                            }
+                            console.log(new_user);
+                        })
+                        .catch((error) => {
+                            var errorCode = error.code;
+                            var errorMessage = error.message;
+                            console.log(errorCode),
+                            console.log(errorMessage)
+                        });
+                    },
+                    signInWithPassword(){
+                        const email = document.getElementById("user-email").value;
+                        const password = document.getElementById("user-password").value;
+                        console.log(email);
+                        console.log(password);
+                        
+                        firebase.auth().signInWithEmailAndPassword(email, password)
+                        .then((userCredential) => {
+                        var user = userCredential.user;
+                        
+                        const new_user = {
+                        displayName: user.displayName,
+                        email: user.email,
+                        photo: user.photoURL
+                        };
+                        data.user = new_user;
+                        data.logged = true;
+                        this.$root.$forceUpdate();
+                        window.location.hash = "/home";
+                        
+                        console.log(new_user);
+                        })
+                        .catch((error) => {
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        console.log(errorCode);
+                        console.log(errorMessage);
+                        });
+                        }
+                };
+
+                //Роути (Які компоненти відображено)
+                const routes = {
+                    '/': Home,
+                    '/home': Home,
+                    '/login': Login
+                }
+
+                const app = {
+                    data() {  return data },
+                    methods: { 
+                        logOut(){
+                            firebase.auth().signOut().then(() => {
+                                data.logged = false;
+                                this.$forceUpdate();
+                                window.location.hash = "/home";
+                              }).catch((error) => {
+                               
+                              });
+                        }
+                
+                    },
+                    components: { },
+                    computed: {
+                        currentView() {
+                            return routes[this.currentPath.slice(1) || '/'] || NotFound
+                        }
+                    },
+                    mounted() {
+                        window.addEventListener('hashchange', () => {
+                            this.currentPath = window.location.hash
+                        })
+                    }
+                }
+                Vue.createApp(app).mount('#app');
+            })
